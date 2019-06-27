b68e740dedb08476b580a1998b14aa205f16c98a
Søren Gregersen <s@grgrsn.dk>
2019-06-27 10:24:59 +0200

    Added template for VIPER presenter test

--- Design Pattern/VIPER Presenter Test.xctemplate/___FILEBASENAME___PresenterTest.swift
+++ Design Pattern/VIPER Presenter Test.xctemplate/___FILEBASENAME___PresenterTest.swift
@@ -4,16 +4,25 @@ import RxSwift
 import SwiftyMocky
 import XCTest
 
-class ParameterPresenterTest: XCTestCase {
+class ___VARIABLE_ModuleName___PresenterTest: XCTestCase {
     private var view: ___VARIABLE_ModuleName___ViewMock!
     private var interactor: ___VARIABLE_ModuleName___UseCaseMock!
     private var router: ___VARIABLE_ModuleName___WireframeMock!
     private var presenter: ___VARIABLE_ModuleName___Presenter!
 
+    private let viewDidLoad = PublishSubject<Void>()
+
     override func setUp() {
         view = ___VARIABLE_ModuleName___ViewMock()
         interactor = ___VARIABLE_ModuleName___UseCaseMock()
         router = ___VARIABLE_ModuleName___WireframeMock()
+
+        Given(view, .didLoad(getter: ControlEvent(events: viewDidLoad)))
+
         presenter = ___VARIABLE_ModuleName___Presenter((view, interactor, router))
+    }
+
+    func testModule() {
+    	
     }
 }
