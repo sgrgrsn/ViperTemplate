0f165caee63c95b8fa58588d38a3175ca9bd9da6
Søren Gregersen <s@grgrsn.dk>
2019-06-27 13:22:26 +0200

    Added plain class for unit test

---
+++ Design Pattern/Unit test.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,12 @@
+@testable import <#AppModule#>
+import XCTest
+
+class ___FILEBASENAMEASIDENTIFIER___: XCTestCase {
+    override func setUp() {
+        
+    }
+
+    func test<#TestName#>() {
+        
+    }
+}
