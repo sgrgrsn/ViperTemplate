a38d22d182c2c14f9947d1d07300ae9bbc63c910
Søren Gregersen <s@grgrsn.dk>
2019-07-09 10:50:51 +0200

    Removed unused files

--- Design Pattern/VIPER Texture Files.xctemplate/___FILEBASENAME___Interactor.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class ___VARIABLE_ModuleName___Interactor {
-    weak var output: ___VARIABLE_ModuleName___InteractorOutput?
-}
-
-extension ___VARIABLE_ModuleName___Interactor: ___VARIABLE_ModuleName___UseCase {
-
-}
